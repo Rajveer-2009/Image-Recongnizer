ed97c0595d5ebc6c80aebdd173c032c947d3d487
Rajveer-2009 <84115638+Rajveer-2009@users.noreply.github.com>
2022-07-26 15:34:53 +0530

    update

--- main.js
+++ main.js
@@ -22,3 +22,19 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded() {
     console.log("modelLoded");
 }
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotresult);
+}
+
+function gotresult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].lable;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3)
+    }
+}
